db4f3775cf6d68ec14c8502ba5aeff11867e4ba1
bdcoller <bdcoller@gmail.com>
2022-09-10 22:36:22 -0500

    time variables are double precision

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ class Program
 {
     static void Main()
     {
-        float t = 0.0f;    // time
-        float tEnd = 3.0f; // ending time
-        float dt = 0.02f;  // time step
+        double t = 0.0;    // time
+        double tEnd = 3.0; // ending time
+        double dt = 0.02;  // time step
 
         // time loop
         while(t < tEnd)
