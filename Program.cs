abddd6794bbc43b8e84a78cad4fc8a69ba0bcb22
bdcoller <bdcoller@gmail.com>
2022-09-15 02:57:54 -0500

    Looks like it might be integrating the ODE.

--- Program.cs
+++ Program.cs
@@ -11,16 +11,42 @@ class Program
         double tEnd = 3.0; // ending time
         double dt = 0.02;  // time step
 
-        //time loop
-        Console.WriteLine(t.ToString());
-        while(t < tEnd - dt*0.5)
+        int n = 1;   // number of ODEs to be integrated
+        double[] x = new double[n];
+        double[] f = new double[n];
+
+        int i;
+        string s = t.ToString();
+        for(i=0;i<n;++i)
         {
-            t = t + dt;
-            Console.WriteLine(t.ToString());
+            x[i] = 0.0;    // set initial condition to zero
+            s += "," + x[i].ToString();
         }
 
-        
+        //time loop
+        Console.WriteLine(s);
+        while(t < tEnd - dt*0.5)
+        {
+            RhsFunc(x,t,f);
+            s = (t+dt).ToString();
+            for(i=0;i<n;++i)
+            {
+                x[i] += f[i] * dt;
+                s+= "," + x[i].ToString();
+            }
+            t = t + dt;
+            Console.WriteLine(s);
+        }
     }
 
-    
+    //------------------------------------------------------------------------
+    // RhsFunc: Calculates the right side of the differential equation
+    //------------------------------------------------------------------------
+    static void RhsFunc(double[] st,double t,double[] f)
+    {
+        double k = 0.9;
+        double Teq = 20.0;
+
+        f[0] = k*(Teq - st[0]);
+    }
 }
