a2ae7e68ae6dfa55fd140d14e3cc85e7a37491e9
bdcoller <bdcoller@gmail.com>
2022-09-19 13:05:30 -0500

    Compare analytic solution

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ class Program
     static void Main()
     {
         double t = 0.0;    // time
-        double tEnd = 3.0; // ending time
-        double dt = 0.02;  // time step
+        double tEnd = 5.0; // ending time
+        double dt = 0.005;  // time step
 
         int n = 1;   // number of ODEs to be integrated
         double[] x = new double[n];
@@ -22,19 +22,22 @@ class Program
             x[i] = 0.0;    // set initial condition to zero
             s += "," + x[i].ToString();
         }
+        s+= "," + x[0].ToString();
 
         //time loop
         Console.WriteLine(s);
         while(t < tEnd - dt*0.5)
         {
-            RhsFunc(x,t,f);
             s = (t+dt).ToString();
+            RhsFunc(x,t,f);
             for(i=0;i<n;++i)
             {
                 x[i] += f[i] * dt;
                 s+= "," + x[i].ToString();
             }
             t = t + dt;
+            double sol = 20.0*(1.0-Math.Exp(-0.9*t));
+            s+= "," + sol.ToString();
             Console.WriteLine(s);
         }
     }
